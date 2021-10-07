cab2ab940ec4b7b1cf5415ac3c8a73831cad95e8
umamahale <umagasmos@gmail.com>
2021-10-07 09:04:53 -0400

    first message

---
+++ Permutation.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alogrithms
+{
+    class Permutation
+    {
+        private static void permute(String str,
+                                int l, int r)
+        {
+            if (l == r)
+                Console.WriteLine(str);
+            else
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    str = swap(str, l, i);
+                    permute(str, l + 1, r);
+                    str = swap(str, l, i);
+                }
+            }
+        }
+
+        /**
+        * Swap Characters at position
+        * @param a string value
+        * @param i position 1
+        * @param j position 2
+        * @return swapped string
+        */
+        public static String swap(String a,
+                                int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);
+            return s;
+        }
+
+        // Driver Code
+        public static void Main()
+        {
+            String str = "ABC";
+            int n = str.Length;
+            permute(str, 0, n - 1);
+        }
+    }
+}
+
